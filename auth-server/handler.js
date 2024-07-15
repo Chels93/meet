87fb4d672d5a76bf56ef585dfb5b7386da3cc851
Chels93 <cacguff@gmail.com>
2024-07-15 17:15:25 +0100

    updated cors

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -13,7 +13,7 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 
 const getResponseHeaders = () => ({
-  "Access-Control-Allow-Origin": "http://127.0.0.1:8080",
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Credentials": true,
   "Access-Control-Allow-Headers": "Content-Type",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
